b1cd09409b6636d1db877c5a88fa4ecb2b4cf3a5
Chris Longhurst <Chris.Longhurst@informa.com>
2024-08-08 15:43:30 +0100

    tidying

--- patd.js
+++ patd.js
@@ -9,4 +9,32 @@ Hooks.once("init", () => {
     CONFIG.Actor.dataModels.warrior = characters.CharacterData;
     CONFIG.Actor.dataModels.boss = characters.CharacterData;
     CONFIG.Actor.dataModels.stooge = characters.CharacterData;
+
+    CONFIG.Actor.trackableAttributes = {
+        playerCharacter: {
+            bar: [currentHealth],
+            value: [healthBars, 
+                tokens.power.value, 
+                tokens.speed.value,
+                tokens.iron.value, 
+                tokens.burning.value, 
+                tokens.weakness.value,
+                tokens.challenge.value, 
+                tokens.fatigue.value, 
+                tokens.control.value,
+                tokens.chaos.value, 
+                tokens.inspiration.value, 
+                tokens.training.value
+            ]
+        },
+        warrior: {
+            
+        },
+        boss: {
+
+        },
+        stooge: {
+
+        }
+    }
   });
